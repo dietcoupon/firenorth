33b888a759ec36737ff094c49c2182368579af0b
Tracy <tracy.cogsdill@gmail.com>
2011-08-30 12:59:35 -0400

    rails, posts skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firelight::Application.config.secret_token = '0f148f759e016250afc9a40477c025aec8a0966efd0043fdd036d34ba2b9c619d5a12a251ac759a9bf6d54400512799fd672d9866641433fb86eb316d7df116b'
